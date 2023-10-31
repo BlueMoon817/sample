6be61999beb2d15f50980428f4032daf6224da75
MunYura <espoir3045@gmail.com>
2023-10-31 14:11:11 +0900

    edit

--- window_event/js/resize.js
+++ window_event/js/resize.js
@@ -12,6 +12,7 @@ let count=0;
 window.addEventListener('resize', debounce(() => {
   currWinW=window.innerWidth;
   if(currWinW!==winW){
+    winW= currWinW;
     count++
     console.log('리사이즈 실행됨'+count);
   }
